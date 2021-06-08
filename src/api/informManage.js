ed963d53ef65c048fda66d4854393a3f367681a1
jackluotong <jackluotong@outlook.com>
2021-06-08 22:52:22 +0800

    add interface.

--- src/api/informManage.js
+++ src/api/informManage.js
@@ -8,3 +8,46 @@ export const getInfoInform = (info) => {
     data: info
   })
 }
+
+export const deleteInfoInform = (info) => {
+  return axios.request({
+    url: `/dcenter/noticeInfo/editd/notice/${id}`,
+    method: 'post',
+    data: info
+  })
+}
+
+export const editInfoInform = (info) => {
+  return axios.request({
+    url: '/dcenter/noticeInfo/editd/notice',
+    method: 'post',
+    data: info
+  })
+}
+
+/*
+    alarm page
+*/
+export const getInfoWarning = (info) => {
+  return axios.request({
+    url: '/dcenter/warningRecord/select/warning',
+    method: 'post',
+    data: info
+  })
+}
+
+export const editInfoWarning = (info) => {
+  return axios.request({
+    url: '/dcenter/warningRecord/add/warning',
+    method: 'post',
+    data: info
+  })
+}
+
+export const deleteInfoWarning = (info) => {
+  return axios.request({
+    url: `/dcenter /warningRecord/del/warning/${id}`,
+    method: 'post',
+    data: info
+  })
+}
---
+++ src/api/weightManage.js
@@ -0,0 +1,33 @@
+import axios from '@/libs/api.request'
+
+export const getWeight = (info) => {
+  return axios.request({
+    url: '/dcenter/weightInfo/select/weight',
+    method: 'post',
+    data: info
+  })
+}
+
+export const addWeight = (info) => {
+  return axios.request({
+    url: '/dcenter /weightInfo/edit/weight',
+    method: 'post',
+    data: info
+  })
+}
+
+export const deleteWeight = (info) => {
+  return axios.request({
+    url: '/dcenter /weightInfo/edit/weight',
+    method: 'post',
+    data: info
+  })
+}
+
+export const editWeight = (info) => {
+  return axios.request({
+    url: '/dcenter/weightInfo//edit/weight',
+    method: 'post',
+    data: info
+  })
+}
