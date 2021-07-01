946a93979b0d954b6754c6c97f7c8eec607c3830
jackluotong@outlook.com <jackluotong@outlook.com>
2021-07-01 09:02:39 +0800

    add

--- src/view/single-page/home/home.vue
+++ src/view/single-page/home/home.vue
@@ -1,7 +1,13 @@
 <style scoped>
 h1{
-    margin: 0 auto;
-    color: rgb(29, 22, 22);
+    text-align: center;
+    background-color: rgb(191, 196, 196);
+    font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    font-size: 4rem;
+    font-weight: 200;
+    color: rgb(94, 6, 6);
+    text-shadow: 5px 5px 5px rgb(0, 0, 0, 1);
+    margin: 20px;
 }
 </style>
 <template>
@@ -11,8 +17,8 @@ h1{
             <h1>
                 中宏保险权限管理
             </h1>
-            <p>展示~</p>
         </header>
+
     </div>
   </div>
 </template>
@@ -33,9 +39,3 @@ export default {
   }
 }
 </script>
-
-<style lang="less">
-.count-style{
-  font-size: 50px;
-}
-</style>
